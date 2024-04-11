9d0f85e82678c794c9bea49027271eba6f1412ac
ArturoDuCas <a00833516@tec.mx>
2024-04-10 21:59:04 -0600

    fix: add missing files

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.userRepository.save(createUserDto);
+  }
+}
