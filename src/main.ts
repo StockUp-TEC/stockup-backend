64639504e76923b3e8f342520e6377ff67fecefc
Santiago Quihui <santiagoquihui@hotmail.com>
2024-04-12 19:06:13 -0600

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-
-  const config = new DocumentBuilder()
-    .setTitle('StockUp API')
-    .setDescription('StockUp API for FSAE Tec Racing')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config, {
-    include: [UsersModule],
-  });
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
