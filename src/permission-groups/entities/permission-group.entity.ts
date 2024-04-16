0c1b373e7881ff001135e76d9ac89768db434a75
Santiago Quihui <santiagoquihui@hotmail.com>
2024-04-16 10:11:16 -0600

    add permissions to permission group

--- src/permission-groups/entities/permission-group.entity.ts
+++ src/permission-groups/entities/permission-group.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Permission } from '../../permissions/entities/permission.entity';
 
 @Entity('PERMISSION_GROUP')
 @ObjectType()
@@ -15,4 +16,10 @@ export class PermissionGroup {
   @Column({ name: 'DESCRIPTION' })
   @Field(() => String)
   description: string;
+
+  @OneToMany(() => Permission, (permission) => permission.permissionGroup, {
+    eager: true,
+  })
+  @Field(() => [Permission], { defaultValue: [] })
+  permissions: Permission[];
 }
