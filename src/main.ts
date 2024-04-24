36e43730d258eb2ca3755852122ac4bb58da198d
Santiago Quihui <santiagoquihui@hotmail.com>
2024-04-24 13:50:07 -0600

    feat: global guards

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GqlAuthGuard } from './auth/gql-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new GqlAuthGuard());
 
   app.enableCors();
 
