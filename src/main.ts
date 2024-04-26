21a602aee5c50b73f083027804c402ec2a22b8e9
Santiago Quihui <santiagoquihui@hotmail.com>
2024-04-25 18:31:50 -0600

    feat: https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GqlAuthGuard } from './auth/gql-auth.guard';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const key = fs.readFileSync('./certs/myserver.key');
+  const cert = fs.readFileSync('./certs/stockup_ddns_net.pem');
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key,
+      cert,
+    },
+  });
   // app.useGlobalGuards(new GqlAuthGuard());
 
   app.enableCors();
