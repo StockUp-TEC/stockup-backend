3217e8fc80e9add93037eefb4c232f38846b14e1
Santiago Quihui <santiagoquihui@hotmail.com>
2024-05-12 16:36:48 -0600

    fix: auth

--- src/auth/gql-auth.guard.ts
+++ src/auth/gql-auth.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
@@ -19,5 +20,10 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
     }
 
     return super.canActivate(context);
+  }
+
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
   }
 }
