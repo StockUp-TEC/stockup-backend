31807a3f7a514f7205b8c4422670454b7137e805
Santiago Quihui <santiagoquihui@hotmail.com>
2024-05-22 11:26:34 -0600

    Update create-task.input.ts

--- src/tasks/dto/create-task.input.ts
+++ src/tasks/dto/create-task.input.ts
@@ -2,6 +2,27 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateTaskInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  description: string;
+
+  @Field({ nullable: true })
+  dueDate: Date;
+
+  @Field(() => Int)
+  statusId: number;
+
+  @Field(() => Int)
+  projectId: number;
+
+  @Field(() => Int, { nullable: true })
+  userId: number;
+
+  @Field(() => Int, { nullable: true })
+  effort: number;
+
+  @Field(() => Int, { nullable: true })
+  priorityId: number;
 }
