3fd1142826ca5f4ea5ffcc93731d99397007befd
Santiago Quihui <santiagoquihui@hotmail.com>
2024-06-05 11:04:32 -0600

    include status.tasks relationship to project query

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -56,6 +56,9 @@ export class ProjectsService {
         background: true,
         history: true,
         division: true,
+        statuses: {
+          tasks: true,
+        },
       },
     });
     if (!project) {
